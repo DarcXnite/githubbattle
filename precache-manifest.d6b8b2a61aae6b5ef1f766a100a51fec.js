05aab9bf69c583a6379f591ddfc900aa95bcdb4e
Davis <davisnguyen98@gmail.com>
2019-10-28 18:59:37 -0500

    Updates

---
+++ precache-manifest.d6b8b2a61aae6b5ef1f766a100a51fec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e178eef90e2ee3332233fc8113ba44a8",
+    "url": "/githubbattle/index.html"
+  },
+  {
+    "revision": "3c606d7513e8ce61110b",
+    "url": "/githubbattle/static/css/main.f3838d42.chunk.css"
+  },
+  {
+    "revision": "0dd322de55419ba81f40",
+    "url": "/githubbattle/static/js/0.03e147a1.chunk.js"
+  },
+  {
+    "revision": "d53f6aa9dd5db34e33b1",
+    "url": "/githubbattle/static/js/3.c2432e4b.chunk.js"
+  },
+  {
+    "revision": "f7d4ac56a2e6d4ac0693",
+    "url": "/githubbattle/static/js/4.743978c6.chunk.js"
+  },
+  {
+    "revision": "ef1eb02d836f5c49a4de",
+    "url": "/githubbattle/static/js/5.5fa86db5.chunk.js"
+  },
+  {
+    "revision": "ed499ca5aa4b6e06c25f",
+    "url": "/githubbattle/static/js/6.94502919.chunk.js"
+  },
+  {
+    "revision": "3c606d7513e8ce61110b",
+    "url": "/githubbattle/static/js/main.87028c62.chunk.js"
+  },
+  {
+    "revision": "6b139dd7026c421d8eed",
+    "url": "/githubbattle/static/js/runtime-main.4b82f599.js"
+  }
+]);
